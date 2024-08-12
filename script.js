688732bf851c48604e7f810041dff1b4524dd6a1
Xxiqian <3047004029@qq.com>
2024-08-12 16:55:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+function toggleMusic1() {
+            var audio = document.getElementById('musicPlayer1');
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        }
+
+        function toggleMusic2() {
+            var audio = document.getElementById('musicPlayer2');
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        }
+
+        function toggleMusic3() {
+            var audio = document.getElementById('musicPlayer3');
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        }
+
+        function toggleMusic4() {
+            var audio = document.getElementById('musicPlayer4');
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        }
+
+        function toggleMusic5() {
+            var audio = document.getElementById('musicPlayer5');
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        }
+
+        function toggleMusic6() {
+            var audio = document.getElementById('musicPlayer6');
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        }
+
+        function toggleMusic7() {
+            var audio = document.getElementById('musicPlayer7');
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        }
+
+        function toggleMusic8() {
+            var audio = document.getElementById('musicPlayer8');
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        }
